97781473573c6d8b1871c07cb05a58d7333c2ec1
Hamza Al-Sarsour <hamze.85@live.com>
2021-05-21 07:32:06 +0300

    Add Download image button with its implementation to get image from URL.

--- app/src/main/java/com/taskfoundation/downloadimage/MainActivity.java
+++ app/src/main/java/com/taskfoundation/downloadimage/MainActivity.java
@@ -2,13 +2,66 @@ package com.taskfoundation.downloadimage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageVIew;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageVIew = findViewById(R.id.imageView);
+    }
+
+    public void downloadImage(View view) {
+        ImageDownloader task = new ImageDownloader();
+        Bitmap myImage;
+        try {
+            myImage = task.execute("https://play-lh.googleusercontent.com/TqbYdgu4go2sUh0bQiA7VC52ArvtbIqR01U5Ggcxj9SC8Uyk45p9_uekBAFUjAJbQXE=s180-rw").get();
+
+            imageVIew.setImageBitmap(myImage);
+
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = connection.getInputStream();
+
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+
+                return bitmap;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
     }
 }
